f50255ee38683293dff2f5dfa0a943ada6ad57c5
APIVerve Team <hello@apiverve.com>
2025-02-21 00:42:22 -0600

    Added new API Endpoints

---
+++ ArticleIdeasGeneratorQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class ArticleIdeasGeneratorQueryOptions {
+public string topic { get; set; }
+}
+
+}
